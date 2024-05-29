9b39746dcf136abb22269cd89c6bed6c6621acc5
Dušan Čolić <dcolic3121rn@raf.rs>
2024-05-29 22:24:46 +0200

    adds cors access control

--- src/main/java/rs/raf/filters/AuthFilter.java
+++ src/main/java/rs/raf/filters/AuthFilter.java
@@ -9,21 +9,21 @@ import rs.raf.services.UserService;
 import javax.inject.Inject;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
 import java.io.IOException;
 import java.util.List;
 
 @Provider
-public class AuthFilter implements ContainerRequestFilter {
+public class AuthFilter implements ContainerRequestFilter, ContainerResponseFilter {
 
     @Inject
     UserService userService;
 
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
-
-//        if (true) return;
 
         if (!this.isAuthRequired(requestContext)) {
             return;
@@ -41,6 +41,13 @@ public class AuthFilter implements ContainerRequestFilter {
         } catch (Exception exception) {
             requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).build());
         }
+    }
+
+    @Override
+    public void filter(ContainerRequestContext containerRequestContext, ContainerResponseContext containerResponseContext) throws IOException {
+        containerResponseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+        containerResponseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+        containerResponseContext.getHeaders().add("Access-Control-Allow-Headers", "Content-Type, Authorization");
     }
 
     private boolean isAuthRequired(ContainerRequestContext req) {
